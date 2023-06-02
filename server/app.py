3394b542845dad1e664ec46526ceca14f6bfce9b
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-06-02 14:57:01 -0500

    Add initial openai integration test

--- server/app.py
+++ server/app.py
@@ -53,13 +53,15 @@ from flask_cors import CORS
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-import json
-import requests
+import openai
 from pathlib import Path
 
 # Load environment variables
 BASE_DIR = Path(__file__).resolve().parent
 load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+# Initialize OpenAI API
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -67,6 +69,57 @@ app = Flask(__name__)
 @app.route('/helloworld', methods=['GET'])
 def hello_world():
     return "Hello, World Doppelganger's working!", 200
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    # Get request body
+    body = request.get_json()
+
+    # Get username
+    username = body['username']
+
+    # Get conversation history
+    conversation_history = body['conversation_history']
+
+    # test return response
+    return jsonify({
+        'chat_response': 'Hello, World!',
+    }), 200
+
+    # Get chat response
+    chat_response = openai.Completion.create(
+        engine='davinci',
+        prompt=f'{conversation_history}\nUsername: {username}\nDoppelganger:',
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=['\n', ' Username:', ' Doppelganger:']
+    )
+
+    # Return chat response
+    return jsonify({
+        'chat_response': chat_response['choices'][0]['text'],
+    }), 200
+
+# Example usage for chat rout in js on the clientside:
+# const response = await fetch('https://api.testapp365.com/chat', {
+#   method: 'POST',
+#   headers: {
+#     'Content-Type': 'application/json',
+#   },
+#   body: JSON.stringify({
+#     username: 'testuser',
+#     conversation_history: [
+#       {'user_message: 'How are you?'},
+#       {'system_message': 'I am good, how are you?'},
+#       {'user_message': 'Doing well. Can you tell me about yourself?'},
+#     ]
+#  }),
+# });
+# const data = await response.json();
+# console.log(data.chat_response);
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
