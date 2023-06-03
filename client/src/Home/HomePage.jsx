1586dd17f953324858c2dfe10c8907c8520a75b8
LoopFruits <Kensworldc@gmail.com>
2023-06-02 20:03:27 -0400

    created a backgdound transparent card in home component

--- client/src/Home/HomePage.jsx
+++ client/src/Home/HomePage.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Home() {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-blue-500">
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
+            <div className="bg-white bg-opacity-25 rounded-lg p-10">
             <h1 className="text-5xl text-white font-bold">Home Page!!!</h1>
+            </div>
         </div>
     )
 }
