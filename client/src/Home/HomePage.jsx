183ac579618444805f15167143a509eb958b125c
KeinoC <keino.chichester@gmail.com>
2023-06-04 02:08:24 -0400

    homepage ui updates

--- client/src/Home/HomePage.jsx
+++ client/src/Home/HomePage.jsx
@@ -1,11 +1,46 @@
 import React from "react";
+import { motion, useAnimation, AnimatePresence } from 'framer-motion';
+import "./HomePage.css";
 
 export default function Home() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
-            <div className="bg-white bg-opacity-25 rounded-lg p-10">
-            <h1 className="text-5xl text-white font-bold">Home Page!!!</h1>
-            </div>
-        </div>
-    )
-}
+        return (
+            <motion.div className="homepage scroll-snap-align-start h-full min-h-screen  items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
+              <AnimatePresence>
+                <motion.div
+                  initial={{ opacity: 0, x: '-100vw' }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ opacity: 1, duration: 2.5, type: 'spring', bounce: 0.25 }}
+                  exit={{ opacity: 0, x: '-100vw', delay: 0 }} // Delay the exit animation by 2 seconds
+                  className="doppelganger"
+                >
+                  <span className="letters">D</span>
+                  <span className="letters">o</span>
+                  <span className="letters">p</span>
+                  <span className="letters">p</span>
+                  <span className="letters">l</span>
+                  <span className="letters">e</span>
+                  <span className="letters">g</span>
+                  <span className="letters">ä</span>
+                  <span className="letters">n</span>
+                  <span className="letters">g</span>
+                  <span className="letters">e</span>
+                  <span className="letters">r</span>
+                </motion.div>
+              </AnimatePresence>
+              <AnimatePresence>
+                <motion.div
+                  className="tag"
+                  initial={{ opacity: 0, x: '100vw' }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ opacity: 1, duration: 2.5, type: 'spring', bounce: 0.25 }}
+                  exit={{ opacity: 0, x: '100vw', delay: 0 }}
+                >
+                  Generate your own personal assistant. You can get its' coffee...
+                </motion.div>
+
+              </AnimatePresence>
+            </motion.div>
+          );
+        }
+    
+
