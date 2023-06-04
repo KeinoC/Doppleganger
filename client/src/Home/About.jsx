130f99e981c5fd657d991fd556c522deb18fdd92
KeinoC <keino.chichester@gmail.com>
2023-06-04 09:49:39 -0400

    pulling for video add

--- client/src/Home/About.jsx
+++ client/src/Home/About.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Team from "./Team"
+import "./About.css"
 
 export default function About() {
     return (
-        <div className="scroll-snap-align-start h-full min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
-            <div className="bg-white bg-opacity-25 rounded-lg p-10">
-            <h1 className="text-5xl text-white font-bold">About Doppelganger</h1>
+        <div className=" h-full min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
+            <div className="about-container bg-white bg-opacity-25 rounded-lg p-10">
+            <h1 className=" about-container mx-auto flex items-center text-5xl text-white font-bold">About Doppelganger</h1>
+            <iframe width="786" height="556" src="https://www.youtube.com/embed/bHH0DvoK7Nw" title="Doppelganger - MLH Hack Your Portfolio 2023" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
         </div>
     )
--- client/src/Home/Demo.jsx
+++ client/src/Home/Demo.jsx
@@ -5,6 +5,7 @@ const Demo = () => {
         <div name="demo" className="w-full h-screen bg-black text-white">
             <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
                 <p className="text-4xl">Demo</p>
+                
             </div>
         </div>
     )
